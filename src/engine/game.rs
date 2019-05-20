5d062d7c076e1491f46036421381bb4b2cbe04d3
Simon Salloum <simon.salloum@izettle.com>
2019-05-20 10:51:33 +0200

    Calculate draw probability

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -2,42 +2,57 @@ use crate::model::team::Team;
 extern crate rand;
 use rand::Rng;
 
-pub struct GameResult {
+pub trait GameResult {
+  fn home_team(&self) -> Team;
+  fn away_team(&self) -> Team;
+  fn home_team_score(&self) -> u16;
+  fn away_team_score(&self) -> u16;
+}
+
+// TODO: implement DrawResult
+pub struct WinLossResult {
   home_team: Team,
   away_team: Team,
   home_team_score: u16,
   away_team_score: u16
 }
 
-impl GameResult {
-  fn home_team(&self) -> &Team {
-    &self.home_team
-  }
-  fn away_team(&self) -> &Team {
-    &self.away_team
-  }
-  fn home_team_score(&self) -> &u16 {
-    &self.home_team_score
-  }
-  fn away_team_score(&self) -> &u16 {
-    &self.away_team_score
-  }
-  fn is_draw(&self) -> bool {
-    &self.home_team_score == &self.away_team_score
+impl WinLossResult {
+  fn winner(&self) -> Team {
+    if self.home_team_score > self.away_team_score {
+      return self.home_team()
+    } else {
+      return self.away_team()
+    }
   }
 }
 
-pub fn game(home_team: Team, away_team: Team) -> GameResult {
+impl GameResult for WinLossResult {
+  fn home_team(&self) -> Team {
+    self.home_team
+  }
+  fn away_team(&self) -> Team {
+    self.away_team
+  }
+  fn home_team_score(&self) -> u16 {
+    self.home_team_score
+  }
+  fn away_team_score(&self) -> u16 {
+    self.away_team_score
+  }
+}
+
+pub fn game(home_team: Team, away_team: Team) -> impl GameResult {
   let num = rand::thread_rng().gen_range(0, 10);
   if num < 5 {
-    GameResult{
+    WinLossResult{
       home_team: home_team,
       away_team: away_team,
       home_team_score: 1,
       away_team_score: 0,
     }
   } else {
-    GameResult{
+    WinLossResult{
       home_team: home_team,
       away_team: away_team,
       home_team_score: 0,
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use rand::prelude::*;
 use rand::distributions::WeightedIndex;
 
 fn main() {
-    println!("Hello, world!");
     let team1 = &mut Team::new("a", 2128.0);
     let team2 = &mut Team::new("b", 2094.0);
     let gr = pick_winner(team1, team2);
@@ -19,6 +18,10 @@ fn pick_winner<'a>(home_team: &'a Team, away_team: &'a Team) -> GameResult {
     let diff_away: f64 = away_team.rating()-home_team.rating();
     let p_home: f64 = win_probability(diff_home);
     let p_away: f64 = win_probability(diff_away);
+    let p_draw = draw_probability(p_home, p_away);
+    println!("p_home: {:?}", p_home);
+    println!("p_away: {:?}", p_away);
+    println!("p_draw: {:?}", p_draw);
 
     let mut rng = thread_rng();
     let probabilities = [(home_team, p_home*10.0), (away_team, p_away*10.0)];
@@ -30,7 +33,6 @@ fn pick_winner<'a>(home_team: &'a Team, away_team: &'a Team) -> GameResult {
         ref t if t.name() == home_team.name() => {
             let ht = Team::update_rating(home_team, match_importance, 1.0, p_home);
             let at = Team::update_rating(away_team, match_importance, 0.0, p_away);
-            println!("{}", home_team.rating());
             GameResult {
                 home_team: ht,
                 away_team: at,
@@ -72,4 +74,9 @@ fn win_probability(diff: f64) -> f64 {
     let exp: f64 = 10.0;
     let c: f64 = 291.5;
     return 1.0/(1.0+exp.powf(-diff/c));
+}
+
+fn draw_probability(p_home: f64, p_away: f64) -> f64 {
+    let normalized_p: f64 = p_home / (p_home + p_away);
+    return (1.0/3.0) * std::f64::consts::E.powf(-f64::powf(normalized_p - 0.5, 2.0) / (2.0 * f64::powf(0.28, 2.0)))
 }
