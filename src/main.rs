a74da15ed756e51923e3dbedef7ca8b78e102900
Simon Salloum <simon.salloum@izettle.com>
2019-05-20 16:38:00 +0200

    initial game loop

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,29 @@ extern crate simmy_dimmy;
 pub use simmy_dimmy::engine::simulation;
 pub use simmy_dimmy::model::team::Team;
 
+use std::io;
+
 fn main() {
     let team1 = &mut Team::new("a", 2128.0);
     let team2 = &mut Team::new("b", 2094.0);
-    let gr = simulation::simulate_game(team1, team2);
+    loop {
+        let gr = simulation::simulate_game(team1, team2);
+        println!("{:?}", gr);
+        println!("Play another match? (Yes/no)");
 
-    println!("{:?}", gr)
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input.trim().to_lowercase().eq("yes") {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+            Err(error) => {
+                println!("error: {}", error);
+                break;
+            }
+        }
+    }
 }
