478a02b3d018d780204e50d5b0af1bb3bea92b88
Simon Salloum <simon.salloum@izettle.com>
2019-05-21 14:15:21 +0200

    add some todos

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,11 @@ fn main() {
         println!("{}", l);
         println!("Play another match? (Yes/no)");
 
+        // todo: simulate all games between all teams (1 home and 1 away vs each team)
+        // todo: save season history (previous winner)
+        // todo: make current ranking dependent on squad (implement team squad, players and player ratings)
+        // todo: player development (ranking increase/decrease based on player age, training and match performance)
+
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
