2ea55db0a2898e7fc2564773d989b6675944755c
Simon Salloum <simon.salloum@izettle.com>
2019-05-24 15:42:26 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ pub use football_simulator::model::league;
 pub use football_simulator::model::league::League;
 pub use football_simulator::model::team::Team;
 
-use std::collections::HashMap;
-use std::io;
+use std::io::{self, Write};
 
-use indicatif::ProgressBar;
+use colored::*;
+use hashbrown::HashMap;
+use indicatif::{ProgressBar, ProgressStyle};
 use itertools::Itertools;
 use uuid::Uuid;
 
@@ -42,91 +43,32 @@ const TEAMS: [(&'static str, f64); 20] = [
 
 lazy_static! {
     static ref HELP_FMT: String = format!(
-        "{0: <20}{1}\n{2: <20}{3}\n{4: <20}{5}\n{6: <20}{7}",
-        "help:",
-        "show this message",
-        "stats <season>:",
-        "show the season stats for the selected season",
-        "winners:",
-        "show the count of all league winners",
+        "{0: <20}{1}\n{2: <20}{3}\n{4: <20}{5}\n{6: <20}{7}\n{8: <20}{9}\n{10: <20}{11}",
+        "command".bold(),
+        "description".bold(),
+        "help",
+        ": show this message",
+        "simulate <seasons>",
+        ": start a new simulation with the given number of seasons",
+        "stats <season>",
+        ": show the season stats for the given season from the previous simulation",
+        "winners",
+        ": show the count of all league winners from the previous simulation",
         "exit:",
         ":'("
     );
     static ref HELP: &'static str = HELP_FMT.as_str();
 }
 
-// todo: refactor fugliness
 fn main() {
     let mut winners: HashMap<&'static str, i64> = HashMap::new();
     let mut standings: HashMap<i64, League> = HashMap::new();
 
-    let mut seasons: u64 = 0;
-    println!("Enter the number of seasons that you want to simulate (min. 1):");
-
-    while seasons <= 0 {
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read the number of seasons");
-        match input.trim().parse::<u64>() {
-            Ok(i) => {
-                if i <= 0 {
-                    println!("Please enter a number greater than 0:");
-                    continue;
-                } else {
-                    seasons = i;
-                }
-            }
-            Err(..) => {
-                println!("Please enter a number greater than 0:");
-                continue;
-            }
-        }
-    }
-
-    let mut teams: HashMap<Uuid, Team> = TEAMS
-        .iter()
-        .map(|t| Team::new(t.0, t.1))
-        .map(|team| (team.id, team))
-        .collect();
-
-    // todo: use https://docs.rs/ndarray/0.12.1/ndarray/ instead? may be able to parallelize
-    let schedule: Vec<(Uuid, Uuid)> = teams
-        .iter()
-        .map(|(_, v)| v.id)
-        .cartesian_product(teams.iter().map(|(_, v)| v.id))
-        .filter(|x| !(x.0 == x.1))
-        .collect();
-    
-    let pb = ProgressBar::new(seasons);
-    for season in 1..seasons + 1 {
-        let mut l = league::League::new(teams.values().collect_vec(), season as i64);
-
-        for t in schedule.iter() {
-            let (result, ht, at) =
-                simulation::simulate_game(*teams.get(&t.0).unwrap(), *teams.get(&t.1).unwrap());
-            teams.insert(ht.id, ht);
-            teams.insert(at.id, at);
-            l.update_standings(result);
-        }
-
-        winners
-            .entry(l.leader().team)
-            .and_modify(|e| *e += 1)
-            .or_insert(1);
-
-        standings.insert(season as i64, l);
-        pb.inc(1);
-
-        // todo: player development (ranking increase/decrease based on player age, training and match performance)
-        // todo: make team ranking dependent on squad (implement team squad, players and player ratings)
-        // parallelize certain operations using Rayon (https://github.com/rayon-rs/rayon)?
-    }
-
-    pb.finish_with_message("All seasons simulated!");
     println!("{}", *HELP);
     let mut exit = false;
     while !exit {
+        print!("football-simulator> ");
+        let _ = io::stdout().flush();
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
@@ -139,6 +81,27 @@ fn main() {
                 w.sort_by(|a, b| b.1.cmp(a.1));
                 for winner in w {
                     println!("{:?}", winner);
+                }
+            }
+            x if x.starts_with("simulate") => {
+                let split: Vec<&str> = x.split_whitespace().collect();
+                match split.get(1) {
+                    Some(arg) => match arg.parse::<u64>() {
+                        Ok(seasons) => {
+                            let pb = ProgressBar::new(seasons);
+                            pb.set_style(ProgressStyle::default_bar().template(
+                                "[{elapsed_precise}] {bar:40.black} {pos:>7}/{len:7} {msg}",
+                            ));
+                            let (w, s) = simulate(seasons, &pb);
+                            winners = w;
+                            standings = s;
+                            pb.finish_with_message(
+                                format!("Finished simulating {} seasons", seasons).as_str(),
+                            );
+                        }
+                        Err(..) => println!("Invalid number of seasons for command 'simulate'"),
+                    },
+                    None => println!("No seasons provided for command 'simulate'"),
                 }
             }
             x if x.starts_with("stats") => {
@@ -162,4 +125,48 @@ fn main() {
         }
     }
     std::process::exit(0);
+}
+
+fn simulate(seasons: u64, pb: &ProgressBar) -> (HashMap<&'static str, i64>, HashMap<i64, League>) {
+    let mut winners: HashMap<&'static str, i64> = HashMap::new();
+    let mut standings: HashMap<i64, League> = HashMap::new();
+
+    let mut teams: HashMap<Uuid, Team> = TEAMS
+        .iter()
+        .map(|t| Team::new(t.0, t.1))
+        .map(|team| (team.id, team))
+        .collect();
+
+    // todo: use https://docs.rs/ndarray/0.12.1/ndarray/ instead? may be able to parallelize
+    let schedule: Vec<(Uuid, Uuid)> = teams
+        .iter()
+        .map(|(_, v)| v.id)
+        .cartesian_product(teams.iter().map(|(_, v)| v.id))
+        .filter(|x| !(x.0 == x.1))
+        .collect();
+
+    for season in 1..seasons + 1 {
+        let mut l = league::League::new(teams.values().collect_vec(), season as i64);
+
+        for t in schedule.iter() {
+            let (result, ht, at) =
+                simulation::simulate_game(*teams.get(&t.0).unwrap(), *teams.get(&t.1).unwrap());
+            teams.insert(ht.id, ht);
+            teams.insert(at.id, at);
+            l.update_standings(result);
+        }
+
+        winners
+            .entry(l.leader().team)
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+
+        standings.insert(season as i64, l);
+        pb.inc(1);
+        // todo: player development (ranking increase/decrease based on player age, training and match performance)
+        // todo: make team ranking dependent on squad (implement team squad, players and player ratings)
+        // parallelize certain operations using Rayon (https://github.com/rayon-rs/rayon)?
+    }
+
+    return (winners, standings);
 }
